ca0fb46c1f8f00fbdb14724a47563c922c709600
Justin <justinbengtson@icloud.com>
2022-09-10 15:36:01 -0600

    Added Adding a photo from Camera option

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -6,22 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     @IBAction func imagePickerAction(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true)
     }
+    
+    @IBAction func cameraIMagePickerAction(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        
+        if AVCaptureDevice.authorizationStatus(for: .video) == .authorized {
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePicker.delegate = self
+                imagePicker.sourceType = .camera
+                present(imagePicker, animated: true)
+            }
+        } else {
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                DispatchQueue.main.async {
+                    if granted {
+                        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                            imagePicker.delegate = self
+                            imagePicker.sourceType = .camera
+                            self.present(imagePicker, animated: true)
+                        }
+                    } else {
+                        // TODO: Permissions not granted take user to settings
+                    }
+                }
+            }
+        }
+    }
+    
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true)
